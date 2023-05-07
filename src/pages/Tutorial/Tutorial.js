a046e11f02e310a295c205853c81061f1bff3449
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-05-07 15:25:52 +0530

    added some updates

--- src/pages/Tutorial/Tutorial.js
+++ src/pages/Tutorial/Tutorial.js
@@ -1,13 +1,13 @@
 import Layout from "../../layout/Layout";
 import Container from "../../components/UI/Container/Container";
+import "./Tutorial.css";
 
 function Tutorial() {
   return (
     <Layout>
       <Container>
         <iframe
-          width="1280"
-          height="720"
+          className="video"
           src="https://www.youtube.com/embed/hwNWx1GTSKo"
           title="8K HDR | The Mirror Dimension (Spider-Man: No Way Home) | Dolby 5.1"
           frameBorder="0"
